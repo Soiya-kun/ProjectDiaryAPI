ebb4fad2df1800d632b473c234e2646903b3ac68
Seiya Ito <42412743+Soiya-kun@users.noreply.github.com>
2018-10-11 16:07:24 +0900

    Delete an unused thing

--- ProjectDiaryAPI/diary/views.py
+++ ProjectDiaryAPI/diary/views.py
@@ -1,71 +1,9 @@
 import django_filters
 from rest_framework import viewsets, filters
 
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import DayCreateForm
 from .models import Day
 from .serializers import DaySerializer
 
-def index(request):
-    context = {
-        'day_list':Day.objects.all(),
-    }
-    return render(request, 'diary/day_list.html', context)
-
-
-
-def add(request):
-    form = DayCreateForm(request.POST or None)
-
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('diary:index')
-
-    context = {
-        'form':form
-    }
-    return render(request, 'diary/day_form.html', context)
-
-
-
-def update(request, pk):
-    day = get_object_or_404(Day, pk=pk)
-
-    form = DayCreateForm(request.POST or None, instance=day)
-
-    if request.method =='POST' and form.is_valid():
-        form.save()
-        return redirect('diary:index')
-
-    context = {
-        'form':form
-    }
-    return render(request, 'diary/day_form.html', context)
-
-
-
-def delete(request, pk):
-    day = get_object_or_404(Day, pk=pk)
-
-    if request.method =='POST':
-        day.delete()
-        return redirect('diary:index')
-
-    context = {
-        'day':day,
-    }
-    return render(request, 'diary/day_confirm_delete.html', context)
-
-
-
-def detail(request, pk):
-    day = get_object_or_404(Day, pk=pk)
-
-
-    context = {
-        'day':day,
-    }
-    return render(request, 'diary/day_detail.html', context)
 
 class DayViewSet(viewsets.ModelViewSet):
     queryset = Day.objects.all()
